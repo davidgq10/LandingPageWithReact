462164a651cf56599166b44c11e7085e166e7439
davidgq10 <davidgq10@hotmail.com>
2021-04-16 18:19:33 +0000

    primera entrega

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,80 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import {
+	Navbar,
+	Container,
+	Jumbotron,
+	Nav,
+	Card,
+	Button
+} from "react-bootstrap";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function MyFooter() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Container className="justify-content-center">
+				<Navbar.Text>Copyright 2021 Website</Navbar.Text>
+			</Container>
+		</Navbar>
 	);
 }
+
+export function MyNav() {
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse
+					id="responsive-navbar-nav"
+					className="justify-content-end">
+					<Nav>
+						<Nav.Link href="#deets">Home</Nav.Link>
+						<Nav.Link href="#deets">Abaut</Nav.Link>
+						<Nav.Link href="#deets">Services</Nav.Link>
+						<Nav.Link eventKey={2} href="#memes">
+							Contact
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+export function MyJumbo() {
+	return (
+		<Container>
+			<Jumbotron className="bg-cover">
+				<h1 className="text-light">Hello, world!</h1>
+				<p className="text-light">
+					This is a simple hero unit, a simple jumbotron-style
+					component for calling extra attention to featured content or
+					information.
+				</p>
+				<p>
+					<Button variant="primary">Learn more</Button>
+				</p>
+			</Jumbotron>
+			<Container fluid></Container>
+		</Container>
+	);
+}
+export function MyCard(props) {
+	return (
+		<Card style={{ width: "18rem" }} className="mb-4">
+			<Card.Img variant="top" src={props.img} />
+			<Card.Body>
+				<Card.Title>{props.title}</Card.Title>
+				<Card.Text>{props.text}</Card.Text>
+				<Button variant="primary">learn more!</Button>
+			</Card.Body>
+		</Card>
+	);
+}
+
+MyCard.propTypes = {
+	title: PropTypes.string,
+	img: PropTypes.string,
+	text: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,58 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { MyNav, MyJumbo, MyCard, MyFooter } from "./component/home.js";
+import { Container, CardDeck } from "react-bootstrap";
+
+function Home() {
+	return (
+		<div>
+			<MyNav />
+			<Container>
+				<MyJumbo />
+				<CardDeck>
+					<MyCard
+						title={"Kamloops Art Page"}
+						text={
+							"Day and Night, by Jiya Singh AE Perry Elementary, Grade 4 I have drawn this picture because it tells a little story about feelings."
+						}
+						img={
+							"https://images.glaciermedia.ca/polopoly_fs/1.24212567.1601479674!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/kamloops-art-page-2.jpg"
+						}
+					/>
+					<MyCard
+						title={"Coronavirus inspire"}
+						text={
+							"How will coronavirus inspire writers, filmmakers, and other creatives? Expect more stories about inequality—and maybe the end of superhero mania, says historian Jennifer Wright."
+						}
+						img={
+							"https://media.vanityfair.com/photos/5e8f9f875752fb00088317c4/16:9/w_1280,c_limit/The-Art-of-Making-Art-About-a-Plague.jpg"
+						}
+					/>
+					<MyCard
+						title={"Shine"}
+						text={
+							"Artist Creates Pencil Drawings That Look Like They’re Lit With Fluorescent Lights"
+						}
+						img={
+							"https://www.paellacreativa.com.ar/wp-content/uploads/2020/01/dibujos_lapiz_luces_fluorescentes_6.jpg"
+						}
+					/>
+					<MyCard
+						title={"Visual art"}
+						text={
+							"Visual arts are a form of art that is being studied all over the world. It has expanded into a career in the recent years. Visual arts are art forms that have many standards."
+						}
+						img={
+							"https://lh3.googleusercontent.com/proxy/422WJ5jU74lcSGme1N6vFXP0DFZ2CYwxki6rt9np1GKEvDZWgyu2GLvqIC5ehXqmsxZH-HvA0n6Pv0e847sE3bO7K2a4rEg4n4V95t6BzqFQh6CEJxqnA5oi4Rw0vNzrBB-RdjuR"
+						}
+					/>
+				</CardDeck>
+			</Container>
+			<MyFooter />
+		</div>
+	);
+}
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
